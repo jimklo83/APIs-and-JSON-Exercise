303428bd9af694be7795e601d35b47bd8345b14d
jimklo83 <jimklo83@mail.com>
2023-11-01 08:02:06 -0500

    Finished

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            var client = new HttpClient();
+            
+            for (int i = 0; i <=  4; i++)
+            {
+                var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+                var kanyeURL = "https://api.kanye.rest/";
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                var kanyeQuote = JObject.Parse(kanyeResponse)["quote"].ToString();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Ron says:");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(ronQuote);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Kanye says:");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(kanyeQuote);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Black;
         }
     }
 }
